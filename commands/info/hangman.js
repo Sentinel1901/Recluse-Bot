dcc42fc02f5412ba2e6897637e1d9b0dbf0387d8
Toxicity <82979070+Sentinel1901@users.noreply.github.com>
2021-07-10 21:54:39 -0500

    Update hangman.js

--- commands/info/hangman.js
+++ commands/info/hangman.js
@@ -9,9 +9,9 @@ module.exports = {
     run : async(client, message, args) => {
         if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send('<:recluse11:827727706959249439> | You need manage messages permission.')
         const channel = message.mentions.channels.first() || message.guild.channels.cache.get(args[0])
-        if(!channel) return message.channel.send('<:recluse8:827723422074535936> Please specify a channel  ')
+        if(!channel) return message.channel.send('Please specify a channel  ')
         const word = args.slice(1).join(" ")
-        if(!word) return  message.channel.send('<:recluse8:827723422074535936>  Please specify a word to guess.')
+        if(!word) return  message.channel.send('Please specify a word to guess.')
 
         const hang = new hangman({
             message: message,
