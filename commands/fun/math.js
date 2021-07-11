bc1e11f66643636f33f63eb0d60903a34c845017
Toxicity <82979070+Sentinel1901@users.noreply.github.com>
2021-07-10 21:19:43 -0500

    Update math.js

--- commands/fun/math.js
+++ commands/fun/math.js
@@ -7,7 +7,7 @@ module.exports = {
   category: "misc",
   run: async (client, message, args) => {
     try {
-      if (!args[0]) return message.channel.send("<:recluse6:827723300457152512> Please Give Me Equation!");
+      if (!args[0]) return message.channel.send("Please Give Me Equation!");
 
       const embed = new MessageEmbed()
         .setColor(`${Color}`)
@@ -17,7 +17,7 @@ module.exports = {
 
       message.channel.send(embed);
     } catch (error) {
-      message.channel.send(` <:recluse3:827723090125258812>  Please Give Me Valid Equation | Try Again Later! `).then(() => console.log(error));
+      message.channel.send(`Please Give Me Valid Equation | Try Again Later! `).then(() => console.log(error));
     }
   }
 };
